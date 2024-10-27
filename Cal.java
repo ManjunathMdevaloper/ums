6b25f3a4205b92735b04a20ee971bf454c5ba3b0
ManjunathMdeveloper <mankemanjunath2003@gmail.com>
2024-10-27 12:48:59 +0530

    developed 3 functions ie add,substract and multi

---
+++ Cal.java
@@ -0,0 +1,15 @@
+class Cal
+{
+	static double add(double n1 , double n2)
+	{
+		return n1+n2;
+	}
+	static double substract(double n1, double n2)
+	{
+		return n1-n2;	
+	}
+	static double multiply(double n1 , double n2)
+	{
+		return n1*n2;	
+	}
+}
